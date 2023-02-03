6362364750c3df7abd8aa2a4640308139ae8a362
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-03 20:14:18 +0300

    Add Answers to Group By Questions part 2

--- aggregations.sql
+++ aggregations.sql
@@ -138,4 +138,43 @@ SELECT a.name,
 FROM accounts a
 JOIN orders o ON a.id = o.account_id
 GROUP BY a.name
-ORDER BY smallest_order
+ORDER BY smallest_order -- GROUP BY PART 2
+/* 1.
+For each account, determine the average amount of each type of paper they purchased across their orders.
+Your result should have four columns - one for the account name and one for the average spent on each of the paper types.
+*/
+SELECT a.name,
+	avg(o.standard_amt_usd) standard,
+	avg(o.poster_amt_usd) poster,
+	avg(o.gloss_amt_usd) gloss
+FROM accounts a
+JOIN orders o ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY standard,
+	poster,
+	gloss;
+
+/* 2.
+Determine the number of times a particular channel was used in the web_events table for each sales rep.
+Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences.
+Order your table with the highest number of occurrences first.
+*/
+SELECT s.name,
+	w.channel,
+	count(w.channel) occurrence
+FROM sales_reps s
+JOIN accounts a ON s.id = a.sales_rep_id
+JOIN web_events w ON a.id = w.account_id
+GROUP BY s.name,
+	w.channel
+ORDER BY occurrence DESC;
+
+--  or
+ -- SELECT s.name, w.channel, COUNT(*) num_events
+-- FROM accounts a
+-- JOIN web_events w
+-- ON a.id = w.account_id
+-- JOIN sales_reps s
+-- ON s.id = a.sales_rep_id
+-- GROUP BY s.name, w.channel
+-- ORDER BY num_events DESC;
