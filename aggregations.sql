2747331c555c682aab6bb71f45f07df154210239
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-03 17:42:02 +0300

    Add solutions to GROUP BY questions

--- aggregations.sql
+++ aggregations.sql
@@ -3,40 +3,33 @@
 /*
 Aggregation is an operation to merge multiple records into single result set. Hence,
 Aggregation operation happens down column using one or more of the following operators,
-SUM, AVG, COUNT, MIN/MAX, GROUP BY, DISTINCT, HAVING, ...	
-While Joins is used to combine data from multiple tables based on common feilds. Hence, 
+SUM, AVG, COUNT, MIN/MAX, GROUP BY, DISTINCT, HAVING, ...
+While Joins is used to combine data from multiple tables based on common feilds. Hence,
 Joins happen accross rows.
 
-*/
-
-/*NULL
+*/ /*NULL
 
 NULL - means no data recorded. it doesn't mean 0 or space. In addition, NULL is
 not value, rather it's a property. so when checking if a record is NULL we don't
 use =, > or < operators rather we use IS or IS NOT. e.g -> WHERE primary_poc IS NULL
-*/
-
-/* 1.
+*/ /* 1.
 Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table.
 This should give a dollar amount for each order in the table.
-*/
-
--- select sum(standard_amt_usd) as standard
+*/ -- select sum(standard_amt_usd) as standard
 -- sum(gloss_amt_usd) as gloss
 
-select standard_amt_usd, gloss_amt_usd,
-(standard_amt_usd + gloss_amt_usd) as Total_amt
-from orders
-;
+SELECT standard_amt_usd,
+	gloss_amt_usd,
+	(standard_amt_usd + gloss_amt_usd) AS total_amt
+FROM orders ;
+
 /*
 2.Find the standard_amt_usd per unit of standard_qty paper.
 Your solution should use both aggregation and a mathematical operator.
 
 */
-
-select sum(standard_amt_usd) / sum(standard_qty)
-from orders;
-
+SELECT sum(standard_amt_usd) / sum(standard_qty)
+FROM orders;
 
 /*
 1. When was the earliest order ever placed? You only need to return the date.
@@ -52,39 +45,56 @@ Your final answer should have 6 values - one for each paper type for the average
 
 6. Via the video, you might be interested in how to calculate the MEDIAN.
 Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
-*/
-
-
-
--- My Solutions
+*/ -- My Solutions
 -- 1.
-select max(occurred_at)
-from orders;
 
--- 2. 
-select occurred_at
-from orders
-order by occurred_at desc
-limit 1;
+SELECT max(occurred_at)
+FROM orders;
+
+-- 2.
+
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at DESC
+LIMIT 1;
 
 -- 6.
 
-select *
-from (
-	select total_amt_usd
-	from orders
-	order by total_amt_usd
-	limit 3456
-) as inside_table
-order by total_amt_usd desc
-limit 2
+SELECT *
+FROM
+	(SELECT total_amt_usd
+		FROM orders
+		ORDER BY total_amt_usd
+		LIMIT 3456) AS inside_table
+ORDER BY total_amt_usd DESC
+LIMIT 2;
 
+/*
+GROUP BY
 
+whenever there is a field in the select statement that's not being aggrigated the quiry expects it to be in the GROUP BY clause.
 
+GROUP BY can be used to aggregate data within subsets of the data. For example,
+grouping for different accounts, different regions, or different sales representatives.
+Any column in the SELECT statement that is not within an aggregator must be in the GROUP BY clause.
+The GROUP BY always goes between WHERE and ORDER BY.
+*/ -- Questions
+/*
+1. Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+*/
+SELECT accounts.name,
+	orders.occurred_at
+FROM accounts
+JOIN orders ON accounts.id = orders.account_id
+ORDER BY orders.occurred_at;
 
+-- 2.
+-- Find the total sales in usd for each account. You should include two columns -
+-- the total sales for each company's orders in usd and the company name.
 
-
-
-
-
-
+SELECT a.name,
+	sum(o.total_amt_usd) total_amt_usd
+FROM accounts a
+JOIN orders o ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY a.name
