7925559745c185616defa2062d2be971a4a7d137
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-03 14:56:42 +0300

    Add answers to challenges

---
+++ aggregations.sql
@@ -0,0 +1,90 @@
+-- What is the difference between Aggregation and Joins in SQL
+-- My response:
+/*
+Aggregation is an operation to merge multiple records into single result set. Hence,
+Aggregation operation happens down column using one or more of the following operators,
+SUM, AVG, COUNT, MIN/MAX, GROUP BY, DISTINCT, HAVING, ...	
+While Joins is used to combine data from multiple tables based on common feilds. Hence, 
+Joins happen accross rows.
+
+*/
+
+/*NULL
+
+NULL - means no data recorded. it doesn't mean 0 or space. In addition, NULL is
+not value, rather it's a property. so when checking if a record is NULL we don't
+use =, > or < operators rather we use IS or IS NOT. e.g -> WHERE primary_poc IS NULL
+*/
+
+/* 1.
+Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table.
+This should give a dollar amount for each order in the table.
+*/
+
+-- select sum(standard_amt_usd) as standard
+-- sum(gloss_amt_usd) as gloss
+
+select standard_amt_usd, gloss_amt_usd,
+(standard_amt_usd + gloss_amt_usd) as Total_amt
+from orders
+;
+/*
+2.Find the standard_amt_usd per unit of standard_qty paper.
+Your solution should use both aggregation and a mathematical operator.
+
+*/
+
+select sum(standard_amt_usd) / sum(standard_qty)
+from orders;
+
+
+/*
+1. When was the earliest order ever placed? You only need to return the date.
+
+2. Try performing the same query as in question 1 without using an aggregation function.
+
+3. When did the most recent (latest) web_event occur?
+
+4. Try to perform the result of the previous query without using an aggregation function.
+
+5. Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order.
+Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+
+6. Via the video, you might be interested in how to calculate the MEDIAN.
+Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
+*/
+
+
+
+-- My Solutions
+-- 1.
+select max(occurred_at)
+from orders;
+
+-- 2. 
+select occurred_at
+from orders
+order by occurred_at desc
+limit 1;
+
+-- 6.
+
+select *
+from (
+	select total_amt_usd
+	from orders
+	order by total_amt_usd
+	limit 3456
+) as inside_table
+order by total_amt_usd desc
+limit 2
+
+
+
+
+
+
+
+
+
+
