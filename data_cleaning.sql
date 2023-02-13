77c71d84f41baf445a7b4e4eedbe297566fa1555
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-13 10:53:12 +0300

    Add alternative solution

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -71,3 +71,12 @@ from (
 	then 1 else 0 end as other
 	from accounts
 ) as t1
+
+-- alternative solutions
+
+SELECT SUM(vowels) vowels, SUM(other) other
+FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                           THEN 1 ELSE 0 END AS vowels, 
+             CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                          THEN 0 ELSE 1 END AS other
+            FROM accounts) t1;
