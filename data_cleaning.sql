0b601edc4ac245bceec4a9842607357faf187953
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-13 10:55:19 +0300

    Add another alternative solution

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -43,7 +43,14 @@ from (
 	from accounts
 ) as sub
 
+-- ALTERNATIVE SOLUTION TO QUESTION #3
 
+SELECT SUM(num) nums, SUM(letter) letters
+FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
+                          THEN 1 ELSE 0 END AS num, 
+            CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
+                          THEN 0 ELSE 1 END AS letter
+         FROM accounts) t1;
 /*
 4. Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel,
 and what percent start with anything else?
